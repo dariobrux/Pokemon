4a6fed4f9e2fd92ad31dae717f3f68e7e3cfcc5e
Dario Bruzzese <dariobrux@gmail.com>
2020-10-20 11:04:20 +0200

    Add pokemon name

--- app/src/main/java/com/technicaltest/app/ui/adapters/PokemonAdapter.kt
+++ app/src/main/java/com/technicaltest/app/ui/adapters/PokemonAdapter.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.technicaltest.app.R
 import com.technicaltest.app.extensions.getIdFromUrl
 import com.technicaltest.app.models.Pokemon
+import java.util.*
 
 
 class PokemonAdapter(private val context : Context, private val items: List<Pokemon>) :
@@ -25,6 +27,8 @@ class PokemonAdapter(private val context : Context, private val items: List<Poke
         val pokemon = items[position]
         val url = String.format(context.getString(R.string.url_pokemon_image), pokemon.url?.getIdFromUrl())
         Glide.with(context).load(url).into(holder.img)
+
+        holder.txtName.text = pokemon.name?.capitalize(Locale.getDefault())
     }
 
     override fun getItemCount(): Int {
@@ -33,5 +37,6 @@ class PokemonAdapter(private val context : Context, private val items: List<Poke
 
     class PostViewHolder(@NonNull itemView: View) : RecyclerView.ViewHolder(itemView) {
         var img: ImageView = itemView.findViewById(R.id.img)
+        var txtName: TextView = itemView.findViewById(R.id.txtName)
     }
 }
