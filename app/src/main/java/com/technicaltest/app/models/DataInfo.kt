bd0466b6c5b0b0e14b67545e4c5ba884c1b311bd
Dario Bruzzese <dariobrux@gmail.com>
2020-10-22 01:45:55 +0200

    Add equals and hashCode methods in models

--- app/src/main/java/com/technicaltest/app/models/DataInfo.kt
+++ app/src/main/java/com/technicaltest/app/models/DataInfo.kt
@@ -14,4 +14,23 @@ class DataInfo {
 
     @SerializedName("results")
     var pokemonList: List<Pokemon>? = null
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as DataInfo
+        if (count != other.count) return false
+        if (next != other.next) return false
+        if (previous != other.previous) return false
+        if (pokemonList != other.pokemonList) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = count ?: 0
+        result = 31 * result + (next?.hashCode() ?: 0)
+        result = 31 * result + (previous?.hashCode() ?: 0)
+        result = 31 * result + (pokemonList?.hashCode() ?: 0)
+        return result
+    }
 }
