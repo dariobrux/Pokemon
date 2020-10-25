81af559e08ac2512594332c808023db69fbdcbd6
Dario Bruzzese <dariobrux@gmail.com>
2020-10-25 12:49:25 +0100

    Add javadoc

--- app/src/test/java/com/technicaltest/app/extensions/ColorTest.kt
+++ app/src/test/java/com/technicaltest/app/extensions/ColorTest.kt
@@ -2,12 +2,21 @@ package com.technicaltest.app.extensions
 
 import android.graphics.Color
 import junit.framework.TestCase
+import org.junit.Test
 
 class ColorTest : TestCase() {
 
-    fun testChangeAlpha() {
+    @Test
+    fun testChangeAlpha1() {
         val color = Color.rgb(80, 80, 80)
         val newColor = color.changeAlpha(80)
         assertEquals(Color.argb(80, 80, 80, 80), newColor)
+    }
+
+    @Test
+    fun testChangeAlpha2() {
+        val color = Color.argb(80, 80, 80, 80)
+        val newColor = color.changeAlpha(0)
+        assertEquals(Color.argb(0, 80, 80, 80), newColor)
     }
 }
