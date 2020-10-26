83c18dab9c08b1b454faa2084b1717770fcbac9c
Dario Bruzzese <dariobrux@gmail.com>
2020-10-26 21:11:12 +0100

    Remove commented code

--- app/src/main/java/com/dariobrux/pokemon/app/PokemonApplication.kt
+++ app/src/main/java/com/dariobrux/pokemon/app/PokemonApplication.kt
@@ -11,34 +11,4 @@ import dagger.hilt.android.HiltAndroidApp
  */
 
 @HiltAndroidApp
-class PokemonApplication : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-
-
-//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-    }
-
-//    /**
-//     * Read the night mode from the DataStore.
-//     * Restore the night theme if in the DataStore is stored night.
-//     * Restore the day theme if in the DataStore is stored day.
-//     */
-//    private fun readNightMode(defaultFunc: () -> Unit) {
-//        supervisorScope {
-//            dataStore.readValue(PreferenceKeys.THEME_NIGHT, {
-//                performThemeChanged()
-//                isNightMode = if (this) {
-//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-//                    true
-//                } else {
-//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-//                    false
-//                }
-//            }, {
-//                defaultFunc.invoke()
-//            })
-//        }
-//    }
-}
+class PokemonApplication : Application()
